50ead512c7e4f6af7b2e1acb3cda4c70262f88e7
fminniti <116181882+fminniti@users.noreply.github.com>
2022-10-19 15:09:33 +0000

    Estructura General del proyecto

--- .ccls-cache/@home@runner@TP10TiposAvanzados/emuleted_port.h
+++ .ccls-cache/@home@runner@TP10TiposAvanzados/emuleted_port.h
@@ -3,19 +3,45 @@
 
 #include <stdint.h>
 
-
 enum{
   PORT_A,     //Port A
   PORT_B,     //Port B
   PORT_D      //Port D
 };
 
-typedef union {
-  int16_t  B :8;
-  int16_t  A :8;
-  int16_t  D :16;
-}Port_t;
+typedef struct{
+  uint16_t D;
+} portD_t;
 
+typedef struct{
+  uint8_t A;
+  uint8_t B;
+} portAB_t;
+
+typedef struct {
+  uint16_t b0 : 1;
+  uint16_t b1 : 1;
+  uint16_t b2 : 1;
+  uint16_t b3 : 1;
+  uint16_t b4 : 1;
+  uint16_t b5 : 1;
+  uint16_t b6 : 1;
+  uint16_t b7 : 1;
+  uint16_t b8 : 1;
+  uint16_t b9 : 1;
+  uint16_t b10 : 1;
+  uint16_t b11: 1;
+  uint16_t b12 : 1;
+  uint16_t b13 : 1;
+  uint16_t b14 : 1;
+  uint16_t b15 : 1;
+}portBit_t;
+
+typedef union{
+  portD_t   portD;
+  portAB_t  portAB;
+  portBit_t portBit;
+} Port_t;
 
 void bitSet (uint8_t id_port , uint8_t bit , Port_t port);
 
